247fb1503b4bfa38fbe58ebb5692954a1aed3aeb
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-01 15:21:07 +1100

    setup of login page with talking to the API

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,41 @@
 
 
 const Login = () => {
+    
+    const handleOnSubmit = async (event) => {
+        event.preventDefault()
+        // const response = await fetch("/api/options");    // GET
+        // let service_options = await response.json()
+        // console.log(service_options)
+        const email = event.target.email.value 
+        const password = event.target.password.value
+        console.log("email: ", email)
+        console.log("password: ", password)
+
+        const options = {
+            method:"POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json;charset=UTF-8"
+            },
+            body: JSON.stringify({"email": email, "password": password})
+        }
+        const loginResponse = await fetch("api/auth/sign_in", options)
+        const user = await loginResponse.json()
+        console.log(user)
+        
+    }
+
     return(
         <>
-        <h1>Login Page</h1>
+            <h1>Login Page</h1>
+            <form onSubmit={handleOnSubmit}>
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" />
+                <label htmlFor="password">password</label>
+                <input type="text" name="password" />
+                <button type="submit">Login</button>
+            </form>
         </>
     )
 }
