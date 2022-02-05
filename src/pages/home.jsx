d12bfd12001137094b1a9375df4b47833deb50bd
Keith Ching <CAS022114@coderacademy.edu.au>
2022-02-05 11:08:53 +1100

    put some space between HTML syntax

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -19,14 +19,17 @@ const Home = (props) => {
             <header style={{display:"flex", justifyContent:"center"}}>
                 <h1 style={{margin:"0px"}}><u>Sydey Motor Service Centre</u></h1>
             </header>
+
             <nav style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>
                 <Link to="/"><h2 style={{padding:"20px", margin:"0px"}}>Car Services</h2></Link>
                 <Link to="/options"><h2 style={{padding:"20px", margin:"0px"}}>About Us</h2></Link>
                 <Link to="/make_booking"><h2 style={{padding:"20px", margin:"0px"}}>Booking</h2></Link>
             </nav>
+
             <main style={{marginTop:"0px", marginRight:"100px", marginBottom:"0px", marginLeft:"100px"}}>
                 {serviceTypes}
             </main>
+            
             {/* <h3>username: {props.userSignedIn && signInContext?.user?.username}</h3> 
             <h6>jwt: {props.userSignedIn && signInContext?.user?.jwt}</h6>  */}
         </>
