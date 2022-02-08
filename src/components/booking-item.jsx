99d1ab602865bbf84fa14ffe468c6bf627fef6d3
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-08 16:33:23 +1100

    basic admin booking page finished

--- src/components/booking-item.jsx
+++ src/components/booking-item.jsx
@@ -1,14 +1,19 @@
 import { Link, useNavigate } from "react-router-dom"
+import { useContext} from "react"
+
 import api from "../config/api";
-// import BookingContext from "../utils/BookingContext";
+import BookingContext from "../utils/BookingContext";
 
 const BookingsItem = ({id, date, comment,}) => {
+
     const navigate = useNavigate();
-    // const { bookingContext, setBookingContext} = useContext(BookingContext)
+    const { bookingContext, setBookingContext} = useContext(BookingContext)
 
     const handleOnClick = async (event) => {
         event.preventDefault()
         const myBooking = await api.getBooking(id)
+        setBookingContext(myBooking)
+        navigate("/booking")
     }
 
 
--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -1,8 +1,23 @@
+import { useContext } from "react"
+
+import BookingContext from "../utils/BookingContext"
+
 
 const Booking = () => {
 
+    const { bookingContext } = useContext(BookingContext)
+
     return (
-        <h1>Booking page</h1>
+        <>
+            <h1>Job: {bookingContext.service_type}</h1>
+            <h3>Date: {bookingContext.date}</h3>
+            <h5>Car: {bookingContext.make} {bookingContext.model}</h5>
+            <h5>First name: {bookingContext.first_name}</h5>
+            <h5>Last name: {bookingContext.last_name}</h5>
+            {bookingContext.paid ? <h6> Invoice is Paid</h6> : <h6>Not paid yet</h6>}
+            {bookingContext.finished ? <h6>Car is ready</h6> : <h6>Car not ready</h6>}
+            <button>Update</button>
+        </>
     )
 
 }
