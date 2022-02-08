e8a885cfdd456a48ca60b130e96e7fd6b711e198
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-08 14:13:51 +1100

    useEffect issue resolved by writing it as suggested by the compiler

--- src/components/booking-item.jsx
+++ src/components/booking-item.jsx
@@ -4,21 +4,6 @@ import api from "../config/api";
 const BookingsItem = ({id, date, comment,}) => {
     const navigate = useNavigate();
 
-
-    // const handleSubmit = async (event) => {
-    //     event.preventDefault()
-    //     const userData = {
-    //         email: data.email,
-    //         password: data.password
-    //     };
-    //     const user = await api.signIn(userData);
-    //     dispatch({type: 'setToken', data: user.jwt});
-    //     dispatch({type: 'setUserSignedIn', data: user.username});
-    //     sessionStorage.setItem("token", user.jwt);
-    //     sessionStorage.setItem("user", user.username);
-    //     navigate("/")
-    // };
-
     const handleOnClick = async (event) => {
         event.preventDefault()
         const myBooking = await api.getBooking(id)
@@ -26,14 +11,12 @@ const BookingsItem = ({id, date, comment,}) => {
 
 
     return (
-
-        <h1>Booking Item</h1>
-        // <div>
-        //     <h3>ID {id}</h3>
-        //     <h4>date {date}</h4>
-        //     <p>comment {comment}</p>
-        //     <button onClick={handleOnClick}>SHOW</button>
-        // </div>
+        <div>
+            <h3>ID {id}</h3>
+            <h4>date {date}</h4>
+            <p>comment {comment}</p>
+            <button onClick={handleOnClick}>SHOW</button>
+        </div>
     )
 
 }
