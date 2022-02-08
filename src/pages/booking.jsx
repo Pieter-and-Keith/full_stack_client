c05997a6c9c210938947e2a28a897ed670ba7d58
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-09 09:20:41 +1100

    added finished and paid button on admin booking page

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+import api from "../config/api"
 
 import BookingContext from "../utils/BookingContext"
 
@@ -10,6 +11,21 @@ const Booking = () => {
 
     const handleBackButton = () => {
         navigate("/admin")
+    }
+
+    const handlePaidButton = async () => {
+        const paidData = {
+            paid: true
+        }
+        const paidStatus = await api.updatePaid(paidData, bookingContext.booking_id)
+        console.log(paidStatus)
+    }
+    const handleFinishedButton = async () => {
+        const finishedData = {
+            finished: true
+        }
+        const finishedStatus = await api.updateFinished(finishedData, bookingContext.booking_id)
+        console.log(finishedStatus)
     }
 
     return (
@@ -24,6 +40,8 @@ const Booking = () => {
             {bookingContext.finished ? <h6>Car is ready</h6> : <h6>Car not ready</h6>}
             {/* <button>Update</button> */}
             <button onClick={handleBackButton}>Back</button>
+            <button onClick={handlePaidButton}>Paid</button>
+            <button onClick={handleFinishedButton}>Finished</button>
         </>
     )
 
