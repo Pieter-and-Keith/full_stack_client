12feb2226f81780d6fac98e04fee42fde280df77
Keith Ching <CAS022114@coderacademy.edu.au>
2022-02-10 10:05:38 +1100

    solved the looping issue on home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ConfirmContext from "./utils/ConfirmContext"
 import BookingContext from "./utils/BookingContext";
+import ServiceCreatedContext from "./utils/ServiceCreatedContext";
 import Home from "./pages/home";
 import About from "./pages/about";
 import SignIn from "./pages/sign-in";
 import SignUp from "./pages/sign-up";
 import UserDetails from "./pages/user-details";
-import OptionPage from "./pages/options";
 import MakeBooking from "./pages/make-booking";
 import ConfirmBooking from "./pages/confirm-booking";
 import Admin from "./pages/admin";
@@ -23,6 +23,7 @@ function App() {
   const [services, setServices] = useState([])
   const [confirmContext, setConfirmContext] = useState([]);
   const [bookingContext, setBookingContext] = useState([]);
+  const [serviceCreatedContext, setserviceCreatedContext] = useState([]);
   
   const initialState = {
     userSignedIn: sessionStorage.getItem("user")  || (""),
@@ -39,29 +40,30 @@ function App() {
       }
     }
     fetchData();
-  }, [services]);
+  }, [serviceCreatedContext]);
 
 
   return (
     <StateContext.Provider value={{store,dispatch}}>
       <ConfirmContext.Provider value={{confirmContext, setConfirmContext}}>
         <BookingContext.Provider value={{bookingContext, setBookingContext}}>
-        <BrowserRouter>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home services={services} />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/sign_in" element={<SignIn />} />
-            <Route path="/sign_up" element={<SignUp />} />
-            <Route path="/user_details" element={<UserDetails />} />
-            <Route path="/options" element={<OptionPage services={services}/>} />
-            <Route path="/make_booking" element={<MakeBooking services={services}/>} />
-            <Route path="/confirm_booking" element={<ConfirmBooking confirmContext={confirmContext}/>} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/admin_option_create" element={<AdminOptionCreate />} />
-            <Route path="/booking" element={<Booking />} />
-          </Routes>
-        </BrowserRouter>
+          <ServiceCreatedContext.Provider value={{serviceCreatedContext, setserviceCreatedContext}}>
+            <BrowserRouter>
+              <Nav />
+              <Routes>
+                <Route path="/" element={<Home services={services} />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/sign_in" element={<SignIn />} />
+                <Route path="/sign_up" element={<SignUp />} />
+                <Route path="/user_details" element={<UserDetails />} />
+                <Route path="/make_booking" element={<MakeBooking services={services}/>} />
+                <Route path="/confirm_booking" element={<ConfirmBooking confirmContext={confirmContext}/>} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin_option_create" element={<AdminOptionCreate />} />
+                <Route path="/booking" element={<Booking />} />
+              </Routes>
+            </BrowserRouter>
+          </ServiceCreatedContext.Provider>
         </BookingContext.Provider>
       </ConfirmContext.Provider>
     </StateContext.Provider>
