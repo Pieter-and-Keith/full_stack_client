b268ef17f30a6ea2442c70d8d412f4205f6e9c99
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-10 14:25:54 +1100

    cypress: added sign up test

--- cypress/integration/sign-up.spec.js
+++ cypress/integration/sign-up.spec.js
@@ -1,6 +1,21 @@
-// sign-up.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+const getInputByLabel = (label) => {
+	return cy
+		.contains("label", label)
+		.invoke("attr", "for")
+		.then((id) => {
+			cy.get("#" + id);
+		});
+};
+
+describe("Basic", () => {
+    it("Login redirects", () => {
+        cy.visit("http://localhost:3001/sign_up")
+        cy.url().should("match",/sign_up/)
+        getInputByLabel("Username").type("new-user@test.com")
+        getInputByLabel("Email").type("newuser@test.com")
+        getInputByLabel("Password").type("password")
+        getInputByLabel("Password Confirmation").type("password")
+        cy.get("button").contains("Sign Up").click()
+        cy.url().should("match",/details/)
+    })
+})
