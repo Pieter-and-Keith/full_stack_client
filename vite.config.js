a480fe97a20fb38e480f1623829a2aa553cf660d
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-11 13:49:50 +1100

    changed settings on the proxy by to localhost

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:3000",
-        target: "https://motor-service.herokuapp.com",
+        target: "http://localhost:3000",
+        // target: "https://motor-service.herokuapp.com",
         changeOrigin: true
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
