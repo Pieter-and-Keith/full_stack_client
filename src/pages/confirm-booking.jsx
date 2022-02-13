5fb1c4415346494bc6da4c9f5fc0fad318a55fb2
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-13 14:13:46 +1100

    ran prettier booking.jsx, confirm-booking.jsx, home.jsx, make-booking.jsx

--- src/pages/confirm-booking.jsx
+++ src/pages/confirm-booking.jsx
@@ -1,49 +1,66 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom"
-import ConfirmContext from "../utils/ConfirmContext"
-import { SignTitle, ConfirmBookingForm, ConfirmBookingTitle, BackgroundImage } from '../components/Styled'
+import { Link } from "react-router-dom";
+import ConfirmContext from "../utils/ConfirmContext";
+import {
+  SignTitle,
+  ConfirmBookingForm,
+  ConfirmBookingTitle,
+  BackgroundImage,
+} from "../components/Styled";
 
 const ConfirmBooking = () => {
-    const {confirmContext} = useContext(ConfirmContext);
-    const confirmBooking = confirmContext.booking
+  const { confirmContext } = useContext(ConfirmContext);
+  const confirmBooking = confirmContext.booking;
 
-    return (
-        <BackgroundImage>
-            <ConfirmBookingTitle><i>Sydney Motor Service Centre</i></ConfirmBookingTitle>
-            <ConfirmBookingForm>
-                <SignTitle><u>Confirm Details</u></SignTitle>
-                <h3>Name: </h3> 
-                    <span>{confirmBooking.first_name} {confirmBooking.last_name}</span>
-                <h3>Email: </h3> 
-                    <span>{confirmBooking.email}</span>
-                <h3>Phone Number: </h3> 
-                    <span>{confirmBooking.phone_number}</span>
-                <h3>Address: </h3> 
-                    <span>{confirmBooking.street_number} {confirmBooking.street_name}, {confirmBooking.suburb}, {confirmBooking.state}, {confirmBooking.postcode}</span>
-                <h3>Selected Service: </h3> 
-                    <span>{confirmBooking.service_type}</span>
-                <h3>Selected Date: </h3> 
-                    <span>{confirmBooking.date}</span>
-                <h3>Car Rego: </h3> 
-                    <span>{confirmBooking.rego}</span>
-                <h3>Car Make: </h3> 
-                    <span>{confirmBooking.make}</span>
-                <h3>Car Model: </h3>
-                    <span>{confirmBooking.model}</span>
-                { confirmBooking.comment ? 
-                    <>
-                        <h3>Additional Comment:</h3>
-                        <span>{confirmBooking.comment}</span>
-                    </> 
-                    :
-                    <></>
-                }
-                <div style={{marginTop:"15px"}}>
-                    <Link to="/"><button style={{margin: "10px"}}>Home</button></Link>
-                </div>
-            </ConfirmBookingForm>
-        </BackgroundImage>
-    );
-}
+  return (
+    <BackgroundImage>
+      <ConfirmBookingTitle>
+        <i>Sydney Motor Service Centre</i>
+      </ConfirmBookingTitle>
+      <ConfirmBookingForm>
+        <SignTitle>
+          <u>Confirm Details</u>
+        </SignTitle>
+        <h3>Name: </h3>
+        <span>
+          {confirmBooking.first_name} {confirmBooking.last_name}
+        </span>
+        <h3>Email: </h3>
+        <span>{confirmBooking.email}</span>
+        <h3>Phone Number: </h3>
+        <span>{confirmBooking.phone_number}</span>
+        <h3>Address: </h3>
+        <span>
+          {confirmBooking.street_number} {confirmBooking.street_name},{" "}
+          {confirmBooking.suburb}, {confirmBooking.state},{" "}
+          {confirmBooking.postcode}
+        </span>
+        <h3>Selected Service: </h3>
+        <span>{confirmBooking.service_type}</span>
+        <h3>Selected Date: </h3>
+        <span>{confirmBooking.date}</span>
+        <h3>Car Rego: </h3>
+        <span>{confirmBooking.rego}</span>
+        <h3>Car Make: </h3>
+        <span>{confirmBooking.make}</span>
+        <h3>Car Model: </h3>
+        <span>{confirmBooking.model}</span>
+        {confirmBooking.comment ? (
+          <>
+            <h3>Additional Comment:</h3>
+            <span>{confirmBooking.comment}</span>
+          </>
+        ) : (
+          <></>
+        )}
+        <div style={{ marginTop: "15px" }}>
+          <Link to="/">
+            <button style={{ margin: "10px" }}>Home</button>
+          </Link>
+        </div>
+      </ConfirmBookingForm>
+    </BackgroundImage>
+  );
+};
 
-export default ConfirmBooking
+export default ConfirmBooking;
--- src/pages/make-booking.jsx
+++ src/pages/make-booking.jsx
@@ -1,113 +1,163 @@
 import { useState, useContext, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom"
-import api from "../config/api"
-import {DropDownBox, SignTitle, BookingForm, SignButton, BackgroundImage, ErrorMessage} from '../components/Styled'
-import ConfirmContext from "../utils/ConfirmContext"
-import {useGlobalState} from '../utils/StateContext'
+import { useNavigate, Link } from "react-router-dom";
+import api from "../config/api";
+import {
+  DropDownBox,
+  SignTitle,
+  BookingForm,
+  SignButton,
+  BackgroundImage,
+  ErrorMessage,
+} from "../components/Styled";
+import ConfirmContext from "../utils/ConfirmContext";
+import { useGlobalState } from "../utils/StateContext";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const MakeBooking = (props) => {
-    const navigate = useNavigate()
-    const {store} = useGlobalState()
-    const {userSignedIn} = store
-    const { setConfirmContext } = useContext(ConfirmContext);
+  const navigate = useNavigate();
+  const { store } = useGlobalState();
+  const { userSignedIn } = store;
+  const { setConfirmContext } = useContext(ConfirmContext);
 
-    const [dateError, setDateError] = useState("")
-    const [optionIdError, setOptionIdError] = useState("")
+  const [dateError, setDateError] = useState("");
+  const [optionIdError, setOptionIdError] = useState("");
 
-    useEffect(() => {
-        if (!userSignedIn){
-            navigate("/sign_in")
-        } 
-    })
-
-    const [option, setOption] = useState("")
-    const [comment, setComment] = useState("")
-    const [transaction] = useState({
-        finished: false,
-        paid: false
-    })
-    const [selectedDate, setSelectedDate] = useState({ format: "MM/DD/YYYY" });
-    const convert = (date, format = selectedDate.format) => {
-        let object = { date, format }
-    
-        setSelectedDate({
-          jsDate: date.toDateString(),
-          ...object
-        })
+  useEffect(() => {
+    if (!userSignedIn) {
+      navigate("/sign_in");
     }
-    const filterDays = (date) => {
-        if (date.getDay() === 0 || date.getDay() === 6) {
-            return false;
-        } else {
-            return true;
-        }
-    }  
+  });
 
-    const handleChange = (e) => {
-        const target = e.target;
-            setComment({
-                [target.name]: target.value
-            });
+  const [option, setOption] = useState("");
+  const [comment, setComment] = useState("");
+  const [transaction] = useState({
+    finished: false,
+    paid: false,
+  });
+  const [selectedDate, setSelectedDate] = useState({ format: "MM/DD/YYYY" });
+  const convert = (date, format = selectedDate.format) => {
+    let object = { date, format };
+
+    setSelectedDate({
+      jsDate: date.toDateString(),
+      ...object,
+    });
+  };
+  const filterDays = (date) => {
+    if (date.getDay() === 0 || date.getDay() === 6) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleChange = (e) => {
+    const target = e.target;
+    setComment({
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      option_id: option,
+      date: selectedDate.jsDate,
+      comment: comment.comment,
+      finished: transaction.finished,
+      paid: transaction.paid,
     };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        const data = {
-            option_id: option,
-            date: selectedDate.jsDate,
-            comment: comment.comment,
-            finished: transaction.finished,
-            paid: transaction.paid
-        } 
-        const booking = await api.createBooking(data);
-        if (!booking.booking_id) {
-            { booking.date? setDateError(booking.date[0]) : setDateError("")}
-            { booking.option_id? setOptionIdError(booking.option_id[0]) : setOptionIdError("")}
-        } else {
-            setConfirmContext({booking});
-            navigate("/confirm_booking")    
-        }
+    const booking = await api.createBooking(data);
+    if (!booking.booking_id) {
+      {
+        booking.date ? setDateError(booking.date[0]) : setDateError("");
+      }
+      {
+        booking.option_id
+          ? setOptionIdError(booking.option_id[0])
+          : setOptionIdError("");
+      }
+    } else {
+      setConfirmContext({ booking });
+      navigate("/confirm_booking");
     }
+  };
 
-    return(
-        <BackgroundImage>
-            <SignTitle>Booking Page</SignTitle>
-            { dateError ? <> <ErrorMessage>Error: Date {dateError}</ErrorMessage> </> : <> </> } 
-            { optionIdError ? <> <ErrorMessage>Error: Service {optionIdError}</ErrorMessage> </> : <> </> }
-            <BookingForm>
-                <div style={{margin:"5px"}}>
-                    <span>Select serivce:</span>
-                    <br></br>
-                    <select onChange={e => setOption(e.target.value)} defaultValue={'DEFAULT'} > 
-                        <option value="DEFAULT" disabled>Please Select</option>
-                        {props.services.map((service, index) => (
-                            <DropDownBox key={index} value={service?.id}>{service?.service_type} ${service?.price}</DropDownBox>
-                        ))}
-                    </select>
-                </div>
-                
-                <div style={{margin:"5px"}}>
-                    <span>Please select the date: </span>
-                    <br></br>
-                    <DatePicker selected={selectedDate.date} onChange={convert} minDate={new Date()} filterDate={filterDays} />
-                </div>
+  return (
+    <BackgroundImage>
+      <SignTitle>Booking Page</SignTitle>
+      {dateError ? (
+        <>
+          {" "}
+          <ErrorMessage>Error: Date {dateError}</ErrorMessage>{" "}
+        </>
+      ) : (
+        <> </>
+      )}
+      {optionIdError ? (
+        <>
+          {" "}
+          <ErrorMessage>Error: Service {optionIdError}</ErrorMessage>{" "}
+        </>
+      ) : (
+        <> </>
+      )}
+      <BookingForm>
+        <div style={{ margin: "5px" }}>
+          <span>Select serivce:</span>
+          <br></br>
+          <select
+            onChange={(e) => setOption(e.target.value)}
+            defaultValue={"DEFAULT"}
+          >
+            <option value="DEFAULT" disabled>
+              Please Select
+            </option>
+            {props.services.map((service, index) => (
+              <DropDownBox key={index} value={service?.id}>
+                {service?.service_type} ${service?.price}
+              </DropDownBox>
+            ))}
+          </select>
+        </div>
 
-                <div style={{margin:"5px"}}>
-                    <label htmlFor="comment">Additional Comment (Optional):</label>
-                    <br></br>
-                    <textarea rows="15" cols="40" type="text" name="comment" value={comment.comment} onChange={handleChange}  />
-                </div>
-                
-                <SignButton>
-                    <Link to="/"><button style={{margin:"10px"}}>Cancel</button></Link>
-                    <button onClick={handleSubmit} style={{margin:"10px"}}>Confirm</button>
-                </SignButton>
-            </BookingForm>
-        </BackgroundImage>
-    )
-}
+        <div style={{ margin: "5px" }}>
+          <span>Please select the date: </span>
+          <br></br>
+          <DatePicker
+            selected={selectedDate.date}
+            onChange={convert}
+            minDate={new Date()}
+            filterDate={filterDays}
+          />
+        </div>
 
-export default MakeBooking
+        <div style={{ margin: "5px" }}>
+          <label htmlFor="comment">Additional Comment (Optional):</label>
+          <br></br>
+          <textarea
+            rows="15"
+            cols="40"
+            type="text"
+            name="comment"
+            value={comment.comment}
+            onChange={handleChange}
+          />
+        </div>
+
+        <SignButton>
+          <Link to="/">
+            <button style={{ margin: "10px" }}>Cancel</button>
+          </Link>
+          <button onClick={handleSubmit} style={{ margin: "10px" }}>
+            Confirm
+          </button>
+        </SignButton>
+      </BookingForm>
+    </BackgroundImage>
+  );
+};
+
+export default MakeBooking;
